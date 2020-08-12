4463940bfc542c1f5fa24f8ae82624b904fa913e
ConfuSomu <emilemeekel+github@gmail.com>
2020-08-12 14:00:11 -0400

    Correct program strings/messages

--- definitions.h
+++ definitions.h
@@ -62,43 +62,50 @@ byte subState = 0; //Breakin: 0 = working, 1 = should take break; Takebreak: 0 =
 
 
 // Program strings
+/* Hex values are character codes for special characters, as defined in the HD44780U datasheet.
+ * Note that the international/european version (ROM code: A02) is required for these characters to display properly. The more common, japanese version, will display greek characters instead of the following.
+ * E0=11100000=à
+ * EA=11101010=ê
+ * E9=11101001=é
+ * E8=11101000=è
+ */
 const char S_MSG[2][4][11][LCD_WIDTH+1] PROGMEM =
                                {{ // Break in… (globalState=0)
                                  {"Travaille produ-", // subState=0
                                   "-ctivement pend-",
                                   "-ant 52 minutes!"},
-                                 {"Tu deverais pre-", // subState=1
+                                 {"Tu devrais pre- ", // subState=1
                                   "-endre une pause",
-                                  "Appuie sur le   ",
+                                  "Appuies sur le  ",
                                   "bouton.         "},
-                                 {"Tu y est presque", // subState=2
+                                 {"Tu y es  presque", // subState=2
                                   "Concentre-toi   ",
-                                  "bien pour ces 20",
-                                  "dernieres  minu-",
+                                  "bien pour ces 9 ",
+                                  "derni\xE8res  minu-",
                                   "-tes. Allez!    "},
-                                 {"Il est extremem-", // subState=3
+                                 {"Il est extr\xEAmem-", // subState=3
                                   "-ent important  ",
                                   "que tu prennes  ",
                                   "une pause pour  ",
-                                  "ton bien-etre.  ",
+                                  "ton bien-\xEAtre.  ",
                                   "Prends la pause ",
                                   "en appuyant sur ",
                                   "le bouton.      "}
                                                      },
-                                { // Take a break… (globalState=0)
+                                { // Take a break… (globalState=1)
                                  {"Prends ta pause ", // subState=0
                                   "de 17 minutes   ",
-                                  "(appuie sur le  ",
+                                  "(appuies sur le ",
                                   "bouton pour rep-",
                                   "-rendre ton     ",
                                   "travail)        "},
                                  {"Faudrait contin-", // subState=1
-                                  "uer a travailler",
-                                  "Aller! appuie   ",
+                                  "uer \xE0 travailler",
+                                  "Allez! appuyez  ",
                                   "sur ce bouton.  "},
-                                 {"N'arrete pas de ", // subState=2
+                                 {"N'arr\xEAte pas de ", // subState=2
                                   "travailler pend-",
-                                  "-ant trop longt-",
-                                  "emps! Tu ne sera",
-                                  "plus concentre. "},
+                                  "-ant trop longte",
+                                  "mps! Tu ne seras",
+                                  "plus concentr/xE9. "},
                                                      }};
